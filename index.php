f743c3d88e734514b6f5ca937179e565a0b2c80f
Ian Chagas Salgado <ianchagassalgado@hotmail.com>
2020-10-10 18:40:06 -0300

    commit páginas dinâmicas com php e criação da página erro
    página de erro desenvolvida no css pelo meu gosto pessoal de estilo, utilizando o conhecimento adquirido e algumas sacadas durante a aula, que é referente a padronizar um footer alinhado no bottom quando não há muito conteúdo para criar um scroll na página

--- index.php
+++ index.php
@@ -41,92 +41,20 @@
         </div> <!-- fecha center cria contexto para responsivo -->
     </header> <!-- fecha cabeçalho -->
     
-    <section class="banner-principal"> <!-- banner -->
-        <div class="overlay"></div> <!-- overlay -->
-        <div class="center"> <!-- abre center cria contexto para responsivo -->
-            <form>
-                <h2>Qual o seu melhor e-mail?</h2>
-                <input type="email" name="email" required>
-                <input type="submit" name="acao" value="Cadastrar">
-            </form>
-        </div> <!-- fecha center cria contexto para responsivo -->
-    </section> <!-- fecha banner -->
+    <?php
 
-    <section class="descricao-autor"> <!-- autor -->
-        <div class="center"> <!-- abre center cria contexto para responsivo -->
-            <div class="w50 left">
-                <h2>Ian Chagas Salgado</h2>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui. tincidunt porttitor feugiat venenatis eros odio eget mauris donec aliquet porta laoreet turpis quam, massa consectetur elementum quisque eros platea ipsum netus luctus potenti mollis. luctus sed phasellus ac nec luctus porta, lectus aliquam facilisis ad nec, convallis blandit felis hendrerit non. condimentum dolor nostra nullam lorem varius, posuere litora duis ornare porttitor, convallis fringilla luctus class. </p>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui. tincidunt porttitor feugiat venenatis eros odio eget mauris donec aliquet porta laoreet turpis quam, massa consectetur elementum quisque eros platea ipsum netus luctus potenti mollis. luctus sed phasellus ac nec luctus porta, lectus aliquam facilisis ad nec, convallis blandit felis hendrerit non. condimentum dolor nostra nullam lorem varius, posuere litora duis ornare porttitor, convallis fringilla luctus class. </p>
-            </div> <!-- w50 -->
-            <div class="w50 left">
-                <!-- Pega imagem do autor -->
-                <img class="right" src="<?php echo INCLUDE_PATH; ?>estilo/images/foto.jpg">
-            </div> <!-- w50 -->
-            <div class="clear"></div>
-        </div> <!-- fecha center cria contexto para responsivo -->
-    </section> <!-- fecha autor -->
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-    <section class="especialidades"> <!-- abre especialidades -->
-        <div class="center"> <!-- abre center cria contexto para responsivo -->
-            <h2 class="title">Especialidades</h2>
-            <div class="w25 left box-especialidade"> <!-- especialidades -->
-                <h3><i class="fab fa-php"></i></h3>
-                <h4>PHP</h4>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</p>
-            </div> <!-- fecha especialidades -->
-            <div class="w25 left box-especialidade"> <!-- especialidades -->
-                <h3><i class="fab fa-html5"></i></h3>
-                <h4>HTML5</h4>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</p>
-            </div> <!-- fecha especialidades -->
-            <div class="w25 left box-especialidade"> <!-- especialidades -->
-                <h3><i class="fab fa-css3-alt"></i></h3>
-                <h4>CSS3</h4>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</p>
-            </div> <!-- fecha especialidades -->
-            <div class="w25 left box-especialidade"> <!-- especialidades -->
-                <h3><i class="fab fa-js-square"></i></h3>
-                <h4>JavaScript</h4>
-                <p>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</p>
-            </div> <!-- fecha especialidades -->
-            <div class="clear"></div>
-        </div> <!-- fecha center cria contexto para responsivo -->
-    </section> <!-- fecha especialidades -->
+        if(file_exists('pages/'.$url.'.php')) {
+            include('pages/'.$url.'.php');
+        }else{
+            $pagina404 = true;
+            include('pages/404.php');
+        }
 
-    <section class="extras"> <!-- abre extras -->
-        <div class="center"> 
-            <div class="w50 left depoimentos-container">
-                <h2 class="title">Depoimentos dos nossos clientes</h2>
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui."</p>
-                    <p class="nome-autor">Ianzin</p>
-                </div>
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui."</p>
-                    <p class="nome-autor">Patricio The Cat</p>
-                </div>
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui."</p>
-                    <p class="nome-autor">Ablublublé</p>
-                </div>
-            </div> <!-- w50 -->
+    ?>
 
-            <div class="w50 left servicos-container">
-                <h2 class="title">Serviços</h2>
-                <div class="servicos">
-                    <ul>
-                        <li>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</li>
-                        <li>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</li>
-                        <li>Lorem ipsum aenean adipiscing mauris quisque, luctus augue feugiat maecenas eleifend quisque, nostra gravida litora fusce. inceptos malesuada velit imperdiet habitasse hac ornare dolor viverra maecenas ullamcorper hac feugiat magna torquent, suscipit id senectus tempus tincidunt orci laoreet aliquam euismod condimentum scelerisque dui.</li>
-                    </ul>
-                </div>
-            </div> <!-- w50 -->
-            <div class="clear"></div>
-        </div>
-    </section> <!-- fecha extras -->
-
-    <footer> <!-- rodapé -->
+    <footer <?php if(isset($pagina404) && $pagina404 == true) echo 'class="footer-fixed"'; ?>> <!-- rodapé -->
         <div class="center">
             <p>Todos os direitos reservados ®</p>
         </div>
---
+++ pages/404.php
@@ -0,0 +1,10 @@
+<section class="erro-404">
+    <div class="center">
+        <h1><i class="fas fa-times"></i></h1>
+        <h2>Erro!</h2>
+        Redirecionamento incorreto.<br>
+        Caso esse erro ocorra, reporte a nossa equipe.
+        <p>Clique aqui para voltar para a home</p>
+        <button><a href="<?php echo INCLUDE_PATH; ?>home">Voltar</a></button>
+    </div>
+</section>
